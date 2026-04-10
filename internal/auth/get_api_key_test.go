ed6f8671b455c3bf431828dd49ce45c61b5f8340
Matias Martello <matias.martello@stonebranch.com>
2026-04-10 13:24:56 -0300

    failing test and test step

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "http://testendpoint.com", nil)
+
+	req.Header.Set("Authorization", "ApiKey your-token")
+
+	got, err := GetAPIKey(req.Header)
+	want := "your-tokens"
+
+	if err != nil {
+		t.Fatalf("got err %v trying to obtain header", err)
+	}
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expectedL %v, got: %v", want, got)
+	}
+}
